08e8a24ae20635bde0c217ab93c367f2c0a320d7
Jun <cmoonjun11@gmail.com>
2023-04-26 18:19:18 +0900

    헤더, 푸터 컴포넌트 생성

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import Footer from "@/src/component/Footer";
+import Header from "@/src/component/Header";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header></Header>
+      <Component {...pageProps} />
+      <Footer></Footer>
+    </div>
+  );
 }
