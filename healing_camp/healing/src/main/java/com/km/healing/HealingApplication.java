33de0f329db98d43253b3381bd91b800db61bf1c
k0905shs <k0905sh@kakao.com>
2023-04-26 15:36:12 +0900

    project init

---
+++ healing_camp/healing/src/main/java/com/km/healing/HealingApplication.java
@@ -0,0 +1,13 @@
+package com.km.healing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealingApplication.class, args);
+    }
+
+}
