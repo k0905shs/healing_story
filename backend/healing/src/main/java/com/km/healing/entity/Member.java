4d056edd2230f5e1e746c6c2f71cc12d04185760
k0905shs <k0905sh@kakao.com>
2023-04-27 11:55:22 +0900

    MemberEntity, BaseTimeEntity 테스트 작성, 패키지 구조 변경

---
+++ backend/healing/src/main/java/com/km/healing/entity/Member.java
@@ -0,0 +1,37 @@
+package com.km.healing.entity;
+
+import com.km.healing.common.category.Mbit;
+import com.km.healing.entity.base.BaseTImeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "loginId", "password", "mbti"})
+public class Member extends BaseTImeEntity {
+    @Column(name = "member_id")
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login_id")
+    private String loginId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "mbti")
+    @Enumerated(EnumType.STRING)
+    private Mbit mbti;
+
+    public void changePassword(final String password) {
+        this.password = password;
+    }
+
+    @Builder
+    public Member(String loginId, String password, Mbit mbit) {
+        super();
+        this.loginId = loginId;
+        this.password = password;
+        this.mbti = mbit;
+    }
+}
---
+++ backend/healing/src/main/java/com/km/healing/entity/base/BaseTImeEntity.java
@@ -0,0 +1,25 @@
+package com.km.healing.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // JPA Entity 클래스들이 해당 추상 클래스를 상속할 경우 createDate, modifiedDate를 컬럼으로 인식
+@EntityListeners(AuditingEntityListener.class) //audting 기능
+public abstract class BaseTImeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
