a2ce2340e6c59a352bed5e3b60289c5e183cf1b3
k0905shs <k0905sh@kakao.com>
2023-05-06 16:32:24 +0900

    PsyQuesRepositorySupportImpl 등록

--- backend/healing/healing-core/src/main/java/com/km/repository/psyQues/PsyQuesRepositorySupportImpl.java
+++ backend/healing/healing-core/src/main/java/com/km/repository/psyQues/PsyQuesRepositorySupportImpl.java
@@ -1,4 +1,21 @@
 package com.km.repository.psyQues;
 
-public class PsyQuesRepositorySupportImpl implements PsyQuesRepositorySupport{
+import com.km.entity.QPsyQues;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class PsyQuesRepositorySupportImpl extends QuerydslRepositorySupport implements PsyQuesRepositorySupport{
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public PsyQuesRepositorySupportImpl(EntityManager em) {
+        super(QPsyQues.class);
+        super.setEntityManager(em);
+        this.jpaQueryFactory = new JPAQueryFactory(em);
+    }
+
 }
